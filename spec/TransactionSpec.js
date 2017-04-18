6d1c164b4e5180eba33739b3c43815d5612552f2
Flora Harvey <fharvey555@gmail.com>
2017-04-18 12:41:11 +0100

    Create Transaction class that takes amount and pass initial test

--- spec/TransactionSpec.js
+++ spec/TransactionSpec.js
@@ -1,1 +1,14 @@
 "strict mode";
+
+describe("Transaction", function() {
+  var transaction;
+
+  beforeEach(function() {
+    transaction = new Transaction(100);
+  });
+
+  it("takes an amount as a parameter", function() {
+    expect(transaction.amount).toEqual(100);
+  });
+
+});
--- src/Transaction.js
+++ src/Transaction.js
@@ -1,1 +1,12 @@
 "strict mode";
+
+(function(exports) {
+
+  function Transaction(amount) {
+    this.amount = amount;
+  }
+
+
+  exports.Transaction = Transaction;
+
+})(this);
