76530c324daaf1a077bd01fa09acd7a284704c6a
Flora Harvey <fharvey555@gmail.com>
2017-04-18 13:48:33 +0100

    Add deposit function

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -11,4 +11,20 @@ describe("Account", function() {
     expect(account.balance).toEqual(0);
   });
 
+  it("initially has an empty transaction history", function() {
+    expect(account.transactions.length).toEqual(0);
+  });
+
+  describe("depositing", function() {
+    it("adds the deposited amount to the account balance", function() {
+      account.deposit(10);
+      expect(account.balance).toEqual(10);
+    });
+
+    it("saves the transaction to the transaction history", function() {
+      account.deposit(10);
+      expect(account.transactions[0].amount).toEqual(10);
+    });
+  });
+
 });
--- src/Account.js
+++ src/Account.js
@@ -4,7 +4,18 @@
 
   function Account() {
     this.balance = 0;
+    this.transactions = [];
   }
+
+  Account.prototype.deposit = function (amount) {
+    var transaction = new Transaction(amount);
+    this.transactions.push(transaction);
+    this._incrementBalance(amount);
+  };
+
+  Account.prototype._incrementBalance = function (amount) {
+    this.balance += amount;
+  };
 
 
   exports.Account = Account;
